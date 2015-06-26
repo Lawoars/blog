f9e0a7e401deb86cd0eee628e102c2a7af2c5f99
Lawoars <lawoars@gmail.com>
2015-06-27 00:44:35 +0300

    Log out ability

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :users
 
   get '/login' => 'sessions#new'
+  delete 'logout' => 'session#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
